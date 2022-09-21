7cefa35f17a11f7ed25537ccbcb81bc1b8f9c2cf
slackbot115 <viniccius.carvalho22@gmail.com>
2022-09-21 14:58:29 -0300

    Add project files.

---
+++ eAgenda.Webapi/ViewModels/VisualizarItemTarefaViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace eAgenda.Webapi.ViewModels
+{
+    public class VisualizarItemTarefaViewModel
+    {
+        public string Titulo { get; set; }
+
+        public string Situacao { get; set; }
+    }
+}
