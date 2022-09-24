32fabb03251a2f4a5f6ffdc2a772e02161a8bc77
slackbot115 <viniccius.carvalho22@gmail.com>
2022-09-24 14:00:25 -0300

    Logs no aspnet core com Serilog

--- eAgenda.Webapi/Program.cs
+++ eAgenda.Webapi/Program.cs
@@ -1,7 +1,9 @@
+using eAgenda.Infra.Logging;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,11 +15,23 @@ namespace eAgenda.Webapi
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            ConfiguracaoLogseAgenda.ConfigurarEscritaLogs();
+
+            Log.Logger.Information("Iniciando o servidor da aplicação e-Agenda...");
+
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception exc)
+            {
+                Log.Logger.Fatal(exc, "O servidor da aplicação e-Agenda parou inesperadamente.");
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
